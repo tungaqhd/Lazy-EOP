d27e0bffeaee7ecfb8dd6b53d9864fa8fb02c65b
Nguyễn Sỹ Tùng <tungaqd@gmail.com>
2020-07-11 11:11:29 +0700

    fix submit button, view answer button, update wait seconds on title

--- scripts/eop.js
+++ scripts/eop.js
@@ -1,71 +1,79 @@
-console.log('EOP Lazy has been injected :))');
+console.log("EOP Lazy has been injected :))");
 
-var submitButton = $('#dSubmit');
-var viewAnswerButoon = $('#dViewAnswer');
+var submitButton = $('button:contains("Hoàn thành")');
+var viewAnswerButoon = $('button:contains("Xem đáp án")');
 
 const doAfter = (job, timer) => {
-    return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-            job()
-            resolve()
-        }, timer)
-    })
-}
-const listenAudio = async() => {
-    let playButtons = $('.fa-play-circle')
-    for (let i = 0; i < playButtons.length; ++i) {
-        await doAfter(() => playButtons[i].click(), 2000)
-        if (i == playButtons.length - 1) {
-            await doAfter(() => submitButton.click(), 2000)
-        }
+  return new Promise(function (resolve, reject) {
+    $("title").html(`Vui lòng đợi ${timer/1000} giây nhóe`);
+    timer -= 1000;
+    const countDown = setInterval(() => {
+      if (timer <= 0) {
+        job();
+        resolve();
+        clearInterval(countDown);
+      } else {
+        timer -= 1000;
+        $("title").html(`Vui lòng đợi ${Math.floor(timer / 1000)} giây nhóe`);
+      }
+    }, 1000);
+  });
+};
+const listenAudio = async () => {
+  let playButtons = $(".fa-play-circle");
+  for (let i = 0; i < playButtons.length; ++i) {
+    await doAfter(() => playButtons[i].click(), 2000);
+    if (i == playButtons.length - 1) {
+      await doAfter(() => submitButton.click(), 2000);
     }
-}
+  }
+};
 
-const autoWrite = async() => {
-    let input = $('.dinline')
+const autoWrite = async () => {
+  let input = $(".dinline");
 
-    input.val('Lazy EOP')
+  input.val("Lazy EOP");
 
-    submitButton.click()
-    await doAfter(() => viewAnswerButoon.click(), 3000)
-    await doAfter(() => viewAnswerButoon.click(), 1000)
+  submitButton.click();
+  await doAfter(() => viewAnswerButoon.click(), 30000);
+  await doAfter(() => viewAnswerButoon.click(), 1000);
 
-    input.each(function(i, e) {
-        $(this).val($(this).attr('placeholder'))
-    })
-    submitButton.click()
-}
+  input.each(function (i, e) {
+    $(this).val($(this).attr("placeholder"));
+  });
+  submitButton.click();
+};
 
-const autoListenAndChoose = async() => {
-    let answers = $('.dvoca.active > .dans > a > .dtip')
-    answers.each(async function() {
-        await doAfter(() => $(this).click(), 1000)
-    })
-}
+const autoListenAndChoose = async () => {
+  let answers = $(".dvoca.active > .dans > a > .dtip");
+  answers.each(async function () {
+    await doAfter(() => $(this).click(), 1000);
+  });
+};
 
-const autoFill = async() => {
-    let answers = $('.ques > div > p > div > ins')
+const autoFill = async () => {
+  let answers = $(".ques > div > p > div > ins");
 
-    answers.click()
-    submitButton.click()
-    
-    await doAfter(() => viewAnswerButoon.click(), 3000)
-    await doAfter(() => console.log('waitting'), 3500)
-    
-    answers = $('.ques > div > p > div > ins')
-    realAnswers = []
-    answers.each(function(i, e) {
-        if($(this).parents().attr('class').includes('checked')) {
-            realAnswers.push(i)
-        }
-    })
-    await doAfter(() => viewAnswerButoon.click(), 1000)
-    for(let i of realAnswers) {
-        answers[i].click()
+  answers.click();
+  submitButton.click();
+
+  await doAfter(() => viewAnswerButoon.click(), 3000);
+  await doAfter(() => console.log("waitting"), 3500);
+
+  answers = $(".ques > div > p > div > ins");
+  realAnswers = [];
+  answers.each(function (i, e) {
+    if ($(this).parents().attr("class").includes("checked")) {
+      realAnswers.push(i);
     }
-    submitButton.click()
-}
+  });
+  await doAfter(() => viewAnswerButoon.click(), 1000);
+  for (let i of realAnswers) {
+    answers[i].click();
+  }
+  submitButton.click();
+};
 
-const smartMode = async() => {
-    alert('Đéo có đâu, sao lười thế')
-}
+const smartMode = async () => {
+  alert("Đéo có đâu, sao lười thế");
+};
